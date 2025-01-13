00e2d6dd88a3047d5470200883c0228da192dfe5
Adinan Cenci <adinancenci@protonmail.com>
2025-01-13 21:18:43 +0000

    issue-2: updated documentation

--- src/JsonLinesIterator.php
+++ src/JsonLinesIterator.php
@@ -6,14 +6,29 @@ use AdinanCenci\FileEditor\FileIterator;
 
 class JsonLinesIterator extends FileIterator
 {
-    protected $associative = false;
+    /**
+     * @var bool
+     *   When true JSON objects will be returned as associative arrays
+     *   when false, JSON objects will be returned as objects.
+     */
+    protected bool $associative = false;
 
+    /**
+     * @param string $fileName
+     *   Absolute path to the file.
+     * @param bool $associative
+     *   If true, JSON objects will be returned as associative arrays,
+     *   otherwise they will be returned as objects.
+     */
     public function __construct(string $fileName, bool $associative = false)
     {
         parent::__construct($fileName);
         $this->associative = $associative;
     }
 
+    /**
+     * \Iterator::current()
+     */
     public function current()
     {
         if (! $this->getHandle()) {
